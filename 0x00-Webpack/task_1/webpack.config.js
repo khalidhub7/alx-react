3e42de7f34f4de6bc0b4e313b26a040c9dbd41c5
khalidhub7 <louguikhalid7@gmail.com>
2025-10-01 09:11:52 +0100

    use Webpack with a config file

--- 0x00-Webpack/task_1/webpack.config.js
+++ 0x00-Webpack/task_1/webpack.config.js
@@ -2,26 +2,10 @@
 const path = require("path");
 
 module.exports = {
-  mode: "production",
   entry: "./js/dashboard_main.js",
 
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "public"),
   },
-
-  /* module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: "babel-loader",
-      },
-    ],
-  },
-  devServer: {
-    static: "./public",
-    hot: false,
-  },
-  plugins: [new HtmlWebpackPlugin()], */
 };
