a197a4b1546564c9c54449105c47b9526f1ab627
khalidhub7 <louguikhalid7@gmail.com>
2025-10-01 08:43:44 +0100

    test

--- 0x00-Webpack/task_1/webpack.config.js
+++ 0x00-Webpack/task_1/webpack.config.js
@@ -1,30 +1,10 @@
-// webpack.config.js
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  // starting file
-  entry: "./js/dashboard_main.js",
-  // output file
+  mode: 'production',
+  entry: './js/dashboard_main.js',
   output: {
     filename: "bundle.js",
-    path: path.resolve(__dirname, "public"),
-  },
-  module: {
-    rules: [
-      {
-        // for JS files
-        test: /\.js$/,
-        exclude: /node_modules/,
-        // transpile with Babel
-        use: "babel-loader",
-      },
-    ],
-  },
-  devServer: {
-    static: "./public", // serve from dist
-    hot: false, // hot reload
-  },
-
-  plugins: [new HtmlWebpackPlugin()],
+    path: path.resolve(__dirname, "public")
+  }
 };
