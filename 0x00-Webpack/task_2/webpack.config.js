a6ccb4e1cfb53eae59e137c4b2cf360b469ec932
khalidhub7 <louguikhalid7@gmail.com>
2025-10-01 20:59:41 +0100

    test

--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -1,38 +1,44 @@
 const path = require("path");
+const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
 
 module.exports = {
-  mode: 'production',
-  entry: './js/dashboard_main.js',
-  performance: {
-    maxAssetSize: 1000000,
-    maxEntrypointSize: 1000000,
-  },
+  mode: "production",
+  entry: path.resolve(__dirname, "js/dashboard_main.js"),
+
   output: {
     filename: "bundle.js",
-    path: path.resolve(__dirname, "public")
+    path: path.resolve(__dirname, "public"),
   },
+
   module: {
     rules: [
+      // handle CSS
       {
-        test: /\.css$/i,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
-      }, 
-      {
-        test: /\.(png|jpe?g|gif)$/i,
-        use: [
-          'file-loader',
-          {
-            loader: 'image-webpack-loader',
-            options: {
-              bypassOnDebug: true,
-              disable: true,
-            },
-          },
-        ],
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
       },
-    ]
-  }
+      // handle images
+      {
+        test: /\.(png|jpg|jpeg|gif|svg)$/,
+        type: "asset/resource",
+        generator: { filename: "images/[name][ext]" },
+      },
+    ],
+  },
+
+  optimization: {
+    minimizer: [
+      new ImageMinimizerPlugin({
+        minimizer: {
+          implementation: ImageMinimizerPlugin.imageminMinify,
+          options: {
+            plugins: ["imagemin-mozjpeg", "imagemin-pngquant"],
+          },
+        },
+      }),
+    ],
+  },
+  performance: {
+    maxAssetSize: 1000000, // 1 MB
+  },
 };
