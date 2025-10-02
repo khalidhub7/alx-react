a1e5b41bf2ed6a56502e6950fa787eef012fc862
khalidhub7 <louguikhalid7@gmail.com>
2025-10-02 15:10:38 +0100

    refactor

--- 0x00-Webpack/task_2/js/dashboard_main.js
+++ 0x00-Webpack/task_2/js/dashboard_main.js
@@ -1,21 +1,29 @@
-import $ from 'jquery';
-import debounce from 'lodash/debounce';
+// check if running in a browser
+const checker = [typeof window, typeof document];
+if (checker.includes("undefined")) {
+  process.exit(0);
+}
 
-$(document).ready(function() {
-	$('<p>').text('Holberton Dashboard').appendTo('body');
-	$('<p>').text('Dashboard data for the students').appendTo('body');
-	$('<button>').text('Click here to get started').appendTo('body');
-	$('<p>').attr('id', 'count').appendTo('body');
-	$('<p>').text('Copyright - Holberton School').appendTo('body');
+import $ from "jquery";
+import { debounce } from "lodash";
+import "../css/main.css";
 
-	let count = 0;
-	const countParagraph = $('#count');
+$(() => {
+  $("body").append('<p id="logo"></p>');
 
-	function updateCounter() {
-		count++;
-		countParagraph.text(`${count} clicks on the button`);
-	}
+  $("body").append("<p>ALX Dashboard</p>");
+  $("body").append("<p>Dashboard data for the students</p>");
+  $("body").append("<button>Click here to get started</button>");
+  $("body").append('<p id="count"></p>');
+  $("body").append("<p>Copyright - ALX</p>");
 
-	const button = $('button');
-	button.on('click', debounce(updateCounter, 300));
+  let count = 0;
+  const updateCounter = () => {
+    count += 1;
+    $("#count").text(`${count} clicks on the button`);
+  };
+
+  const debouncedFunc = debounce(updateCounter, 500);
+
+  $("button").on("click", debouncedFunc);
 });
--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -1,33 +1,28 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-	mode: 'production',
-	entry: './js/dashboard_main.js',
-	output: {
-		filename: 'bundle.js',
-		path: path.resolve(__dirname, 'public')
-	},
-	module: {
-		rules: [
-			{
-				test: /\.css$/,
-				use: ['style-loader', 'css-loader'],
-			},
-			{
-				test: /\.jpg$/,
-				use: [
-					'file-loader',
-					{
-						loader: 'image-webpack-loader',
-						options: {
-							mozpeg: {
-								progressive: true,
-								quality: 75,
-							},
-						},
-					},
-				],
-			},
-		],
-	},
+  mode: "production",
+  entry: path.resolve(__dirname, "js/dashboard_main.js"),
+
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "public"),
+  },
+
+  module: {
+    rules: [
+      // handle CSS
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      // handle images
+      {
+        test: /\.(png|jpg|jpeg|gif|svg)$/,
+        use: ["file-loader", "image-webpack-loader"],
+      },
+    ],
+  },
+  // 1 MB
+  // performance: { maxAssetSize: 1000000 },
 };
