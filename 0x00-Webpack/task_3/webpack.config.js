bad9be1ce81f3b21c44c214d2409f5767dc4be40
khalidhub7 <louguikhalid7@gmail.com>
2025-10-02 22:26:48 +0100

    dev servers, modules, and tree shaking

--- 0x00-Webpack/task_3/webpack.config.js
+++ 0x00-Webpack/task_3/webpack.config.js
@@ -1,43 +1,55 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  mode: "development",
-  devtool: "inline-source-map",
+  mode: 'development',
+  entry: {
+    all: ["./modules/header/header.js", "./modules/body/body.js", "./modules/footer/footer.js"],
+    /*header: './modules/header/header.js',
+    body: './modules/body/body.js',
+    footer: './modules/footer/footer.js'*/
+  },
+  performance: {
+    maxAssetSize: 1000000,
+    maxEntrypointSize: 1000000,
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'public')
+  },
+  devtool: 'inline-source-map',
   devServer: {
-    static: path.resolve(__dirname, "dist"),
+    contentBase: path.join(__dirname, './public'),
+    compress: true,
     port: 8564,
   },
-  entry: {
-    header: path.resolve(__dirname, "modules/header/header.js"),
-    body: path.resolve(__dirname, "modules/body/body.js"),
-    footer: path.resolve(__dirname, "modules/footer/footer.js"),
-  },
-
-  output: {
-    filename: "[name].bundle.js",
-    path: path.resolve(__dirname, "dist"),
-  },
-
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin(),
+  ],
   module: {
     rules: [
-      // handle CSS
       {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
-      },
-      // handle images
+        test: /\.css$/i,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }, 
       {
-        test: /\.(png|jpg|jpeg|gif|svg)$/,
-        use: ["file-loader", "image-webpack-loader"],
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              bypassOnDebug: true,
+              disable: true,
+            },
+          },
+        ],
       },
-    ],
-  },
-  // 1 MB
-  // performance: { maxAssetSize: 1000000 },
-  plugins: [new HtmlWebpackPlugin(), new CleanWebpackPlugin()],
-  optimization: {
-    splitChunks: { chunks: "all" },
-  },
+    ]
+  }
 };
