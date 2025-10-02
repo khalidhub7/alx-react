4f52660d271f810234cbd9092a869d11f162618f
khalidhub7 <louguikhalid7@gmail.com>
2025-10-02 15:03:51 +0100

    test

--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -1,28 +1,38 @@
 const path = require("path");
 
 module.exports = {
-  mode: "production",
-  entry: path.resolve(__dirname, "js/dashboard_main.js"),
-
+  mode: 'production',
+  entry: './js/dashboard_main.js',
+  performance: {
+    maxAssetSize: 1000000,
+    maxEntrypointSize: 1000000,
+  },
   output: {
     filename: "bundle.js",
-    path: path.resolve(__dirname, "public"),
+    path: path.resolve(__dirname, "public")
   },
-
   module: {
     rules: [
-      // handle CSS
       {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
-      },
-      // handle images
+        test: /\.css$/i,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }, 
       {
-        test: /\.(png|jpg|jpeg|gif|svg)$/,
-        use: ["file-loader", "image-webpack-loader"],
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              bypassOnDebug: true,
+              disable: true,
+            },
+          },
+        ],
       },
-    ],
-  },
-  // 1 MB
-  // performance: { maxAssetSize: 1000000 },
+    ]
+  }
 };
