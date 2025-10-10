0dd330716508a89ab7d2ffa484d5e21cfac581eb
khalidhub7 <louguikhalid7@gmail.com>
2025-10-10 14:22:03 +0100

    setup for github pages

---
+++ 0x01-react_intro/task_4/dashboard/src/utils.js
@@ -0,0 +1,18 @@
+const getFullYear = () => new Date().getFullYear();
+
+const getFooterCopy = (isIndex) => {
+  if (isIndex === true) {
+    return "ALX";
+  } else if (isIndex === false) {
+    return "ALX main dashboard";
+  }
+};
+
+const getLatestNotification = () =>
+  "<strong>Urgent requirement</strong> - complete by EOD";
+
+export {
+  getFooterCopy,
+  getFullYear,
+  getLatestNotification,
+};
