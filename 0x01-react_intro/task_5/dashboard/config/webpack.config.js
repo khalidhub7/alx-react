4350885f0dca8415697645d199fb6324fc7b4573
khalidhub7 <louguikhalid7@gmail.com>
2025-10-11 02:21:53 +0100

    setup Webpack

--- 0x01-react_intro/task_5/dashboard/config/webpack.config.js
+++ 0x01-react_intro/task_5/dashboard/config/webpack.config.js
@@ -1,44 +1,36 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+// const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: path.join(__dirname, '/dist'),
-        output: {
-            filename: 'bundle.js',
-            path: path.resolve(__dirname, 'dist'),
-        },
-        devtool: 'inline-source-map',
-        devServer: {
-            static: './dist',
-            hot: true,
-        },
-        Plugins: [
-            new HtmlWebpackPlugin({
-                template: './dist/index.html',
-                favicon: './assets/favicon.ico',
-            }),
-        ],
-        module: {
-            rules: [
-                {
-                    test: /\.js$/,
-                    exclude: /node_modules/,
-                    use: ['babel-loader'],
-                },
-                {
-                    test: /\.css$/,
-                    use: ['style-loader', 'css-loader'],
-                },
-                {
-                    test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                    use: ['file-loader'],
-                    options: {
-                        outputPath: 'images',
-                    },
-                },
-            ],
-        },
-    },
+  mode: "development",
+  devtool: "inline-source-map",
+  devServer: { static: "./dist", open: true, hot: true },
+
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      // handle css
+      { test: /\.css$/, use: ["style-loader", "css-loader"] },
+      // handle js
+      /* {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      }, */
+      // handle images
+      {
+        test: /\.(png|jpg|jpeg|gif|svg)$/,
+        use: ["file-loader", "image-webpack-loader"],
+      },
+    ],
+  },
+
+  // plugins: [new HtmlWebpackPlugin(), new CleanWebpackPlugin()]
+  plugins: [new HtmlWebpackPlugin()],
+  // optimization: { splitChunks: { chunks: "all" } },
 };
