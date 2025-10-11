cf94ea05b5be1c0a392ed50eafe09466542f8730
khalidhub7 <louguikhalid7@gmail.com>
2025-10-11 02:12:44 +0100

    setup Webpack

---
+++ 0x01-react_intro/task_5/dashboard/config/webpack.config.js
@@ -0,0 +1,36 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+// const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  devtool: "inline-source-map",
+  devServer: { static: "./dist", open: true, hot: true },
+
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      // handle css
+      { test: /\.css$/, use: ["style-loader", "css-loader"] },
+      // handle js
+      /* {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      }, */
+      // handle images
+      {
+        test: /\.(png|jpg|jpeg|gif|svg)$/,
+        use: ["file-loader", "image-webpack-loader"],
+      },
+    ],
+  },
+
+  // plugins: [new HtmlWebpackPlugin(), new CleanWebpackPlugin()]
+  plugins: [new HtmlWebpackPlugin()],
+  // optimization: { splitChunks: { chunks: "all" } },
+};
