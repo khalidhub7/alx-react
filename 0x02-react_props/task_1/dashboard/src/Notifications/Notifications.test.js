a76f8bd76623ebe0a5a0a2da1480f8c0202ece5d
khalidhub7 <louguikhalid7@gmail.com>
2025-10-16 05:45:08 +0100

    add tests for each component (rtl not enzyme)

---
+++ 0x02-react_props/task_1/dashboard/src/Notifications/Notifications.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import Notifications from './Notifications';
+
+test('test Notifications component', () => {
+  const { container } = render(<Notifications />);
+
+  expect(
+    container.querySelectorAll(
+      '.Notifications > ul:first-of-type > li',
+    ),
+  ).toHaveLength(3);
+
+  expect(
+    container.querySelector('.Notifications > p').textContent,
+  ).toBe('Here is the list of notifications');
+});
