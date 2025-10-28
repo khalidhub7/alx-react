9f14338923a53d0c40a0bd21c4da6fffd09a22de
khalidhub7 <louguikhalid7@gmail.com>
2025-10-28 07:37:19 +0100

    test

---
+++ 0x03-React_component/task_1/dashboard/src/App/App.js
@@ -0,0 +1,73 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Login from "../Login/Login";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+import CourseList from "../CourseList/CourseList";
+import { getLatestNotification } from "../utils/utils";
+import Notifications from "../Notifications/Notifications";
+
+const listCourses = [
+  { id: 1, name: "ES6", credit: 60 },
+  { id: 2, name: "Webpack", credit: 20 },
+  { id: 3, name: "React", credit: 40 },
+];
+const listNotifications = [
+  { id: 1, type: "default", value: "New course available" },
+  { id: 2, type: "urgent", value: "New resume available" },
+  {
+    id: 3,
+    html: { __html: getLatestNotification() },
+    type: "urgent",
+  },
+];
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleKeyDown = this.handleKeyDown.bind(this);
+  }
+
+  handleKeyDown(event) {
+    if (event.ctrlKey && ["h", "H"].includes(event.key)) {
+      alert("Logging you out");
+      this.props.logOut();
+    }
+  }
+
+  componentDidMount() {
+    document.addEventListener("keydown", this.handleKeyDown);
+  }
+  componentWillUnmount() {
+    document.removeEventListener("keydown", this.handleKeyDown);
+  }
+
+  render() {
+    return (
+      <>
+        <Notifications listNotifications={listNotifications} />
+        <Header />
+        {this.props.isLoggedIn ? (
+          <div className="courses">
+            <CourseList listCourses={listCourses} />
+          </div>
+        ) : (
+          <Login />
+        )}
+        <Footer />
+      </>
+    );
+  }
+}
+
+App.propTypes = {
+  isLoggedIn: PropTypes.bool,
+  logOut: PropTypes.func,
+};
+App.defaultProps = {
+  isLoggedIn: false,
+  logOut: () => {},
+};
+
+export { listNotifications, listCourses }; // for tests
+export default App;
---
+++ 0x03-React_component/task_1/dashboard/src/Notifications/NotificationItem.test.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import NotificationItem from './NotificationItem';
+
+describe('NotificationItem tests', () => {
+  test('renders without crashing', () => {
+    render(<NotificationItem />);
+  });
+
+  test(
+    'renders NotificationItem correctly for both (html, value) props',
+    () => {
+      const scenarios = [
+        { type: 'default', html: { __html: '<u>test</u>' }, value: undefined },
+        { type: 'default', html: undefined, value: 'test_notif' },
+      ];
+      const expected = ['test', 'test_notif'];
+
+      scenarios.forEach((p, i) => {
+        const { container } = render(
+          <NotificationItem
+            type={p.type}
+            html={p.html}
+            value={p.value}
+          />,
+        );
+        expect(
+          container.querySelector('li').textContent,
+        ).toEqual(expected[i]);
+      });
+    },
+  );
+});
