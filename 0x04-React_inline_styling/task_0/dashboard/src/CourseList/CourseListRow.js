2898ae0bc838f22dac1cb339799dead9ff46a649
khalidhub7 <louguikhalid7@gmail.com>
2025-10-31 06:31:00 +0100

    react inline styling + test

---
+++ 0x04-React_inline_styling/task_0/dashboard/src/CourseList/CourseListRow.js
@@ -0,0 +1,42 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const headerStyle = { backgroundColor: "#deb5b545" };
+const rowStyle = { backgroundColor: "#f5f5f5ab" };
+
+const CourseListRow = ({
+  isHeader = false,
+  textFirstCell,
+  textSecondCell = null,
+}) => {
+  return (
+    <tr style={isHeader ? headerStyle : rowStyle}>
+      {isHeader ? (
+        textSecondCell ? (
+          <>
+            <th>{textFirstCell}</th>
+            <th>{textSecondCell}</th>
+          </>
+        ) : (
+          <th colSpan={2}>{textFirstCell}</th>
+        )
+      ) : (
+        <>
+          <td>{textFirstCell}</td>
+          <td>{textSecondCell}</td>
+        </>
+      )}
+    </tr>
+  );
+};
+
+CourseListRow.propTypes = {
+  isHeader: PropTypes.bool,
+  textFirstCell: PropTypes.string.isRequired,
+  textSecondCell: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.number,
+  ]),
+};
+
+export default CourseListRow;
---
+++ 0x04-React_inline_styling/task_0/dashboard/src/CourseList/CourseListRow.test.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import '@testing-library/jest-dom';
+import { render } from '@testing-library/react';
+import CourseListRow from './CourseListRow';
+
+describe('test CourseListRow component', () => {
+  /*
+  whats test:
+      when header is true (one cell)
+      when header is true (two cells)
+      when header is false
+  */
+  test(
+    'renders CourseListRow correctly for header and regular rows',
+    () => {
+      const scenarios = [
+        <thead>
+          <CourseListRow isHeader textFirstCell="test_header" />
+        </thead>,
+        <thead>
+          <CourseListRow
+            isHeader
+            textFirstCell="test_header1"
+            textSecondCell="test_header2"
+          />
+        </thead>,
+        <tbody>
+          <CourseListRow
+            isHeader={false}
+            textFirstCell="test_header1"
+            textSecondCell="test_header2"
+          />
+        </tbody>,
+      ];
+      // in expected each arg is [selector, result]
+      const expected = [
+        ['th', ['colspan', '2']], ['th', 2], ['tr td', 2],
+      ];
+      const expectedStyles = ['#deb5b545', '#deb5b545', '#f5f5f5ab'];
+
+      scenarios.forEach((s, i) => {
+        const { container } = render(<table>{s}</table>);
+        if (Array.isArray(expected[i][1])) {
+          expect(
+            container.querySelector(expected[i][0]),
+          ).toHaveAttribute(...expected[i][1]);
+        } else if (Number.isInteger(expected[i][1])) {
+          expect(
+            container.querySelectorAll(expected[i][0]).length,
+          ).toBe(expected[i][1]);
+        }
+        // test styles
+        expect(container.querySelector('table tr'))
+          .toHaveStyle(`background-color: ${expectedStyles[i]}`);
+      });
+    },
+  );
+});
