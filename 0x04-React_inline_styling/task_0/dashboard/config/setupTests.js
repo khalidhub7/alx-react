4a23ed181e1bc02d4935235fdb87dc230c7a7407
khalidhub7 <louguikhalid7@gmail.com>
2025-10-31 06:38:57 +0100

    react inline styling + test

--- 0x04-React_inline_styling/task_0/dashboard/config/setupTests.js
+++
@@ -1,7 +0,0 @@
-import "@testing-library/jest-dom";
-
-// sets up enzyme to work with react 16
-
-/* import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-configure({ adapter: new Adapter() }); */
