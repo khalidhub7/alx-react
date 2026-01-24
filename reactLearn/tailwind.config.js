6c3746371e18ec73b3e109b364210d9dbf5551ae
khalidhub7 <louguikhalid7@gmail.com>
2026-01-24 14:05:46 +0100

    add custom design token

--- reactLearn/tailwind.config.js
+++ reactLearn/tailwind.config.js
@@ -1,5 +1,20 @@
-export default {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  theme: { extend: {} },
-  plugins: [], // about ready styles (to learn later)
+const obj = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./learn-space/tailwind/*"],
+  plugins: [],
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        primary: "#0000FF", // blue
+      },
+      boxShadow: {
+        soft: "0 4px 12px rgba(0, 0, 0, 0.1)",
+      },
+      fontFamily: {
+        main: ["Inter", "sans-serif"],
+      },
+    },
+  },
 };
+
+export default obj;
