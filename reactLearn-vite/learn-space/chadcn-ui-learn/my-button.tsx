0d72cd19fef97a756ff647888bb4956a90f5bcf4
khalidhub7 <louguikhalid7@gmail.com>
2026-02-05 11:59:13 +0100

    up

--- reactLearn-vite/learn-space/chadcn-ui-learn/my-button.tsx
+++ reactLearn-vite/learn-space/chadcn-ui-learn/my-button.tsx
@@ -1,5 +1,7 @@
 import { Button } from "@/components/ui/button";
 
+/* type 1: Simple (Stateless) Components */
+
 const myButton = () => (
   <div className="h-screen w-screen flex items-center justify-center">
     <Button>click me</Button>
