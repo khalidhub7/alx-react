ce7f4e5920e9415a4aada76e5df18db49b650b7e
khalidhub7 <louguikhalid7@gmail.com>
2026-04-17 08:40:16 +0100

    up

--- nextLearn/read.sh
+++ nextLearn/read.sh
@@ -2,7 +2,7 @@ npx create-next-app@latest app-basics
 prettier --write --print-width 80 file
 
 
-tree ../app-basics/ -I 'node_modules/' > struct.txt
+tree ../app-basics/ -I 'node_modules/' > docs/structure.txt
 
 npx shadcn@latest init
 npx shadcn@latest add avatar
